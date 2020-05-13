3566280921da5c095cf2ee133d393f5cdfd724bb
Rion Hutsell <st.gish@gmail.com>
2020-05-13 10:57:26 -0600

    Complete Iteration 3 and add methods rooms_sorted_by_area and rooms_by_category two tests aren't passing

--- lib/house.rb
+++ lib/house.rb
@@ -12,4 +12,44 @@ class House
   def add_room(room)
     @rooms << room
   end
+
+  def above_market_average?
+    if @price.gsub("$","").to_i > 5000000
+      true
+    else
+      false
+    end
+  end
+
+  def rooms_from_category(category)
+    selected_rooms = []
+
+    @rooms.each do |room|
+      if room.category == category
+        selected_rooms << room
+      end
+    end
+    selected_rooms
+  end
+
+  def details
+    {"price" => @price.gsub("$","").to_i, "address" => @address}
+  end
+
+  def price_per_square_foot
+    total_square_feet = 0.0
+    @rooms.each do |room|
+      total_square_feet += room.area
+    end
+
+    (@price.gsub("$","").to_i / total_square_feet).round(2)
+  end
+
+  def rooms_sorted_by_area
+    (@rooms.sort_by { |room| room.area}).reverse
+  end
+
+  # def rooms_by_category
+  #   @rooms.sort_by { |room| room.category}
+  # end
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -25,7 +25,78 @@ class HouseTest < Minitest::Test
     room_2 = Room.new(:bedroom, 11, '15')
 
     house.add_room(room_1)
-    house.add_room(room_2)   
+    house.add_room(room_2)
     assert_equal [room_1, room_2], house.rooms
+  end
+
+  def test_above_market_average
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal false, house.above_market_average?
+  end
+
+  def test_rooms_from_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+    assert_equal [room_4], house.rooms_from_category(:basement)
+  end
+
+  def test_it_can_display_details
+    skip
+    house = House.new("$400000", "123 sugar lane")
+
+    house.details
+  end
+
+  def test_it_can_show_price_per_square_foot
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal 210.53, house.price_per_square_foot
+  end
+
+  def test_it_can_show_rooms_sorted_by_area
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_4)
+    house.add_room(room_1)
+    house.add_room(room_3)
+    house.add_room(room_2)
+    assert_equal [room_4, room_3, room_2, room_1], house.rooms_sorted_by_area
+  end
+
+  def test_it_can_show_rooms_by_category
+
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_4)
+    house.add_room(room_1)
+    house.add_room(room_3)
+    house.add_room(room_2)
+
   end
 end
