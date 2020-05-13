b710152f152b1b27ef2dccd17281edc604131046
Rion Hutsell <st.gish@gmail.com>
2020-05-13 09:30:06 -0600

    Complete Iteration 2

--- test/house_test.rb
+++ test/house_test.rb
@@ -11,11 +11,21 @@ class HouseTest < Minitest::Test
     assert_instance_of House, house
   end
 
-  def test_it_has_a_price
+  def test_it_has_readable_attributes
     house = House.new("$400000", "123 sugar lane")
 
     assert_equal "$400000", house.price
+    assert_equal "123 sugar lane", house.address
+    assert_equal [], house.rooms
   end
 
+  def test_it_can_add_room
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
 
+    house.add_room(room_1)
+    house.add_room(room_2)   
+    assert_equal [room_1, room_2], house.rooms
+  end
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -16,22 +16,22 @@ class RoomTest < Minitest::Test
 
   def test_it_can_get_area
 
-    room1 = Room.new(:bedroom, 10, '13')
-    room2 = Room.new(:living_room, 15, '12')
-    assert_equal 130, room1.area
-    assert_equal 180, room2.area
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:living_room, 15, '12')
+    assert_equal 130, room_1.area
+    assert_equal 180, room_2.area
   end
 
   def test_it_starts_unpained
-    room1 = Room.new(:bedroom, 10, '13')
+    room_1 = Room.new(:bedroom, 10, '13')
 
-    assert_equal false, room1.is_painted?
+    assert_equal false, room_1.is_painted?
   end
 
   def test_it_can_be_painted
-    room1 = Room.new(:bedroom, 10, '13')
+    room_1 = Room.new(:bedroom, 10, '13')
 
-    room1.paint 
-    assert_equal true, room1.is_painted?
+    room_1.paint
+    assert_equal true, room_1.is_painted?
   end
 end
